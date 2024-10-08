06109827ed11774e36be305c36e2e2365c0a25ab
Your Name <you@example.com>
2024-10-08 15:52:31 -0300

    final

--- foro/views.py
+++ foro/views.py
@@ -1,6 +1,10 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from .models import Noticia
 
 def noticia(request):
     noticias=Noticia.objects.all()
-    return render(request,"foro/foro.html",{'noticias':noticias})
+    paginator = Paginator(noticias, 3)  # se cambia a noticaS para que sea iterable Y DE 3 POR PAGINA
+    page_number = request.GET.get("page") 
+    page_obj = paginator.get_page(page_number) 
+    return render(request, "foro/foro.html", {"page_obj": page_obj}) 
